27633dd4ef0d1714b244bd32ab63f79e2f67762d
AlexeyShoshu <aliakseishoshu@gmail.com>
2023-06-24 19:55:31 +0300

    24.06

--- index.php
+++ index.php
@@ -4,27 +4,37 @@ $connDatabase = new mysqli("localhost", "root", "root", "", "3306");
 if ($connDatabase->connect_error) {
     die("Ошибка: " . $connDatabase->connect_error);
 }
-if (!$connDatabase->query("CREATE DATABASE IF NOT EXISTS db")) {
+if (!$connDatabase->query("CREATE DATABASE IF NOT EXISTS db122")) {
     die("База данных не была создана!");
 }
-$connTable = new mysqli("localhost", "root", "root", "db", "3306");
+$connTable = new mysqli("localhost", "root", "root", "db122", "3306");
 if ($connTable->connect_error) {
     die("Ошибка: " . $connTable->connect_error);
 }
-$createTable = "CREATE TABLE IF NOT EXISTS people (surname VARCHAR(30), name VARCHAR(30),  patronymic VARCHAR(30), birth_date DATE, gender BOOLEAN, phone VARCHAR(30), mail VARCHAR(100), height INT, weight FLOAT(1))";
+$createTable = "CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, surname VARCHAR(30), name VARCHAR(30),  patronymic VARCHAR(30), birth_date DATE, gender BOOLEAN, phone VARCHAR(30), mail VARCHAR(100), height INT, weight FLOAT(1))";
 if (!$connTable->query($createTable)) {
     die("Таблица не была создана!");
 }
 
 $showAll = "SELECT * FROM people";
 
+$id = 1;
+$findParameter = 'phone';
+$findValue = '+375295618065';
+$updateParameter = 'surname';
+$updateValue = 'Головашко';
+$showRowParameter = 'name';
+$showRowValue = 'Игорь';
 $surname = 'Прохоров';
 $name = 'Игорь';
 $patronymic = 'Андреевич';
-$birth_date = '1988-08-12'; // перевод введеной даты в тип sql
+$birth_date = date("Y-m-d", strtotime('19.12.2003'));
 $gender = 1; // отображение пола для людей если запрашивается
 $phone = '+375294447744'; // проверка белорусского телефона
-$mail = 'prohor@gmail.com'; // проверка мэйла
+$mail = 'prohor@gmail.com';
+if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    die("Неверно указана почта!");
+}
 $height = 180;
 $weight = 77.2;
 
@@ -43,9 +53,10 @@ function addFirstInfo($connTable, $showAll)
 
 function showInfo($result)
 {
-    echo "<table><tr><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Дата рождения</th><th>Пол</th><th>Телефон</th><th>Почта</th><th>Рост</th><th>Вес</th></tr>";
+    echo "<table><tr><th>ID</th><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Дата рождения</th><th>Пол</th><th>Телефон</th><th>Почта</th><th>Рост</th><th>Вес</th></tr>";
     foreach ($result as $row) {
         echo "<tr>";
+        echo "<td>" . $row["id"] . "</td>";
         echo "<td>" . $row["surname"] . "</td>";
         echo "<td>" . $row["name"] . "</td>";
         echo "<td>" . $row["patronymic"] . "</td>";
@@ -69,9 +80,9 @@ function showAllTables($connTable, $showAll)
     }
 }
 
-function showRow($connTable)
+function showRow($connTable, $showRowParameter, $showRowValue)
 {
-    $showOne = "SELECT * FROM people WHERE surname='Шошу'"; // динамический выбор
+    $showOne = "SELECT * FROM people WHERE {$showRowParameter} ='{$showRowValue}'";
     if ($result = $connTable->query($showOne)) {
         showInfo($result);
     } else {
@@ -87,25 +98,25 @@ function addInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $ma
     }
 }
 
-function updateInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable) // динамическое обновление не всей информации
+function updateInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable, $updateParameter, $updateValue) // динамическое обновление не всей информации -> новые переменные и их проверка с массивом??
 {
-    $updateOne = "UPDATE people SET surname = '{$surname}', name = '{$name}', patronymic = '{$patronymic}', birth_date = '{$birth_date}', gender = {$gender}, phone = '{$phone}', mail = '{$mail}', height = {$height}, weight = {$weight} WHERE name = 'Андрей'";
+    $updateOne = "UPDATE people SET surname = '{$surname}', name = '{$name}', patronymic = '{$patronymic}', birth_date = '{$birth_date}', gender = {$gender}, phone = '{$phone}', mail = '{$mail}', height = {$height}, weight = {$weight} WHERE {$updateParameter} = '{$updateValue}'";
     if (!$connTable->query($updateOne)) {
         echo "Ошибка: " . $connTable->error;
     }
 }
 
-function deleteInfo($surname, $connTable) // удаление одного а не нескольких (привзяка по ID -> добавление переменной выбора ID -> проверка ID или его вытягивание?)
+function deleteInfo($id, $connTable)
 {
-    $deleteOne = "DELETE FROM people WHERE surname='{$surname}'";
+    $deleteOne = "DELETE FROM people WHERE id = {$id}";
     if (!$connTable->query($deleteOne)) {
         echo "Ошибка: " . $connTable->error;
     }
 }
 
-function findInfo($connTable)
+function findInfo($findParameter, $findValue, $connTable)
 {
-    $findInfo = "SELECT * FROM people WHERE phone LIKE '+375295618065'"; // универсальность поиска
+    $findInfo = "SELECT * FROM people WHERE {$findParameter} LIKE '{$findValue}'";
     if ($result = $connTable->query($findInfo)) {
         showInfo($result);
     } else {
@@ -115,9 +126,9 @@ function findInfo($connTable)
 
 addFirstInfo($connTable, $showAll);
 showAllTables($connTable, $showAll);
-// showRow($connTable);
-// addInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable);
-// updateInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable);
-// deleteInfo($surname, $connTable);
-// findInfo($connTable);
+showRow($connTable, $showRowParameter, $showRowValue);
+addInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable);
+updateInfo($surname, $name, $patronymic, $birth_date, $gender, $phone, $mail, $height, $weight, $connTable, $updateParameter, $updateValue);
+deleteInfo($id, $connTable);
+findInfo($findParameter, $findValue, $connTable);
 
